31570b64aa74a6d38c7a3df1abc813b86b22e6d0
DvirShahala <61511562+DvirShahala@users.noreply.github.com>
2023-02-26 21:52:53 +0200

    fix routes stracture

--- trip-manager-server/src/routes/index.ts
+++ trip-manager-server/src/routes/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { geoRoute } from "./geo";
 import { tripRoute } from "./trips";
 import { weatherRoute } from "./weather";
 
@@ -6,3 +7,4 @@ export const routes = express.Router();
 
 routes.use("/trip", tripRoute);
 routes.use("/weather", weatherRoute);
+routes.use("/geo", geoRoute);
--- trip-manager-server/src/routes/weather.ts
+++ trip-manager-server/src/routes/weather.ts
@@ -22,20 +22,3 @@ weatherRoute.get("/maxMinTemp", async (req: Request, res: Response) => {
     res.send(e);
   }
 });
-
-weatherRoute.get("/coordinates", async (req: Request, res: Response) => {
-  try {
-    const address = req.query.address;
-
-    const { data } = await axios.get(
-      `${process.env.BASE_GEOCODING_API_URL}?access_key=${process.env.GEOCODING_API_ACCESS_KEY}&query=${address}`
-    );
-
-    res.status(200);
-    res.send(data);
-  } catch (e) {
-    console.error("error in get coordinates", e);
-    res.status(404);
-    res.send(e);
-  }
-});
