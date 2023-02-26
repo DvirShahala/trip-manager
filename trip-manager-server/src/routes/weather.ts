f691bb2d4b833a9200bd2cc1fa40ac563af720fc
DvirShahala <61511562+DvirShahala@users.noreply.github.com>
2023-02-26 21:50:56 +0200

    add geocoding api

--- trip-manager-server/src/routes/weather.ts
+++ trip-manager-server/src/routes/weather.ts
@@ -11,7 +11,7 @@ weatherRoute.get("/maxMinTemp", async (req: Request, res: Response) => {
     const endDate = req.query.endDate;
 
     const { data } = await axios.get(
-      `${process.env.BASE_API_URL}?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto&current_weather=true&start_date=${startDate}&end_date=${endDate}`
+      `${process.env.BASE_WEATHER_API_URL}?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto&current_weather=true&start_date=${startDate}&end_date=${endDate}`
     );
 
     res.status(200);
@@ -19,6 +19,23 @@ weatherRoute.get("/maxMinTemp", async (req: Request, res: Response) => {
   } catch (e) {
     console.error("error in weather by location", e);
     res.status(400);
+    res.send(e);
+  }
+});
+
+weatherRoute.get("/coordinates", async (req: Request, res: Response) => {
+  try {
+    const address = req.query.address;
+
+    const { data } = await axios.get(
+      `${process.env.BASE_GEOCODING_API_URL}?access_key=${process.env.GEOCODING_API_ACCESS_KEY}&query=${address}`
+    );
+
+    res.status(200);
+    res.send(data);
+  } catch (e) {
+    console.error("error in get coordinates", e);
+    res.status(404);
     res.send(e);
   }
 });
